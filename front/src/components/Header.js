f9bb5ca8f3dde494fab4b8f2a26c3ccdd561ef58
danielmarques28 <danielmarques7@hotmail.com>
2021-02-05 00:30:46 -0300

    Create the structure of Header component

---
+++ front/src/components/Header.js
@@ -0,0 +1,11 @@
+import styles from '../styles/scss/components/Header.module.scss';
+
+function Header() {
+  return (
+    <header className={styles['app-header']}>
+      Hello Header
+    </header>
+  );
+}
+
+export default Header;
