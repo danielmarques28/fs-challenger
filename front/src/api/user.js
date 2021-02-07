466ac10b897c6d8ad9ec95698f14cbfc0c36c41d
danielmarques28 <danielmarques7@hotmail.com>
2021-02-07 11:55:30 -0300

    feat: Add route to login endpoint and update others routes of user endpoints

--- front/src/api/user.js
+++ front/src/api/user.js
@@ -1,20 +1,25 @@
 import http from './http';
 
+function login(body) {
+  const url = `/users/login`;
+  return http.post(url, body);
+}
+
 function index() {
-  const url = `/users`;
+  const url = `/users/all_users_except_current_user`;
   return http.get(url);
 }
 
-function getRemainingBrooches(id) {
-  const url = `/users/${id}/remaining_brooches`;
+function getRemainingBrooches() {
+  const url = `/users/remaining_brooches`;
   return http.get(url);
 }
 
-function getAllInteractions(id) {
-  const url = `/users/${id}/all_interactions`;
+function getAllInteractions() {
+  const url = `/users/all_interactions`;
   return http.get(url);
 }
 
-const endpoints = { index, getRemainingBrooches, getAllInteractions };
+const endpoints = { login, index, getRemainingBrooches, getAllInteractions };
 
 export default endpoints;
