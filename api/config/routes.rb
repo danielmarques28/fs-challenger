c6fed9791958b7d1a85af6f246643f345bed483f
danielmarques28 <danielmarques7@hotmail.com>
2021-02-07 00:17:42 -0300

    refactor: Change back-end routes to use resources

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,15 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope(:path => '/api') do
     # Users
-    get '/users', to: 'users#index'
+    resources :users, only: [:index]
     get '/users/:id/remaining_brooches', to: 'users#remaining_brooches'
     get '/users/:id/all_interactions', to: 'users#all_interactions'
 
     # UserBrooches
-    get '/user_brooches', to: 'user_brooches#index'
-    post '/user_brooches', to: 'user_brooches#create'
+    resources :user_brooches, only: [:index, :create]
 
     # Brooches
-    get '/brooches', to: 'brooches#index'
+    resources :brooches, only: [:index]
   end
 end
