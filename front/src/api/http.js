7aee1ddd283077d885e838d279e931e2973fde1b
danielmarques28 <danielmarques7@hotmail.com>
2021-02-07 11:53:53 -0300

    feat: Add redirect to login page if response status is 401

--- front/src/api/http.js
+++ front/src/api/http.js
@@ -1,11 +1,30 @@
 import axios from 'axios';
+import history from '../history';
 
 const http = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL + 'api/',
-  timeout: 8000,
+  timeout: 5000,
   headers: {
     'Content-Type': 'application/json'
   }
+});
+
+http.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('token');
+  if (token)
+    config.headers['Authorization'] = `Bearer ${token}`;
+
+  return config;
+});
+
+http.interceptors.response.use(function (response) {
+  return response;
+}, function (error) {
+  if (error.response.status === 401) {
+    history.push('/login');
+    return error;
+  }
+  return Promise.reject(error);
 });
 
 export default http;
