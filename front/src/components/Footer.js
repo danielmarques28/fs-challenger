389a230f6c050a10f682c044c779fdd4fbe27703
danielmarques28 <danielmarques7@hotmail.com>
2021-02-08 01:49:11 -0300

    feat: Create button in Footer to go to top of the Home page

--- front/src/components/Footer.js
+++ front/src/components/Footer.js
@@ -1,5 +1,16 @@
+import styles from '../styles/scss/components/Footer.module.scss';
+
 export default function Footer() {
+  function goToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
   return (
-    <div></div>
+    <div
+      className={styles['footer']}
+      onClick={goToTop}
+    >
+      voltar ao topo
+    </div>
   );
 }
