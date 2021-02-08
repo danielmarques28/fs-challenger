480789504464d15e45c24b9631ed60b9b3387220
danielmarques28 <danielmarques7@hotmail.com>
2021-02-08 13:42:55 -0300

    feat: Add presence validation in UserBrooch model

--- api/app/models/user_brooch.rb
+++ api/app/models/user_brooch.rb
@@ -18,7 +18,11 @@ end
 
 class UserBrooch < ApplicationRecord
   validates_with UserBroochValidator
-  validates :user_id, uniqueness: { scope: :friend_id }
+  validates :user_id, :friend_id, :brooch_id, presence: true
+  validates :user_id, uniqueness: {
+    scope: :friend_id, message: 'You already gave a brooch to that friend'
+  }
+
   belongs_to :self_user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :friend_user, class_name: 'User', foreign_key: 'friend_id'
   belongs_to :brooch
