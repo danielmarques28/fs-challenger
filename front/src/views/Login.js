daea0965db264c5125cc9c5778dbb3b984711368
danielmarques28 <danielmarques7@hotmail.com>
2021-02-07 21:34:35 -0300

    feat: Add error message in Login Card

--- front/src/views/Login.js
+++ front/src/views/Login.js
@@ -9,6 +9,7 @@ import { useHistory } from 'react-router-dom';
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   let history = useHistory();
 
@@ -22,45 +23,63 @@ export default function Login() {
 
   function handleSubmit(event) {
     event.preventDefault();
+    setError('');
     UserAPI.login({ email, password })
       .then(function (response) {
         localStorage.token = response.data.token;
-        // return <Redirect to="/" />;
         history.push('/');
       })
-      // .catch(function (error) {
-      //   error.response.data;
-      // });
+      .catch(function (error) {
+        setError(error.response.data.error);
+      });
   }
 
   return (
     <div className={styles['login']}>
       <Card style={{ width: '20rem', padding: '15px' }}>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              onChange={handleChangeEmail}
-              value={email}
-              type="email"
-              placeholder="Enter email"
-            />
-          </Form.Group>
+        <Card.Body>
+          <Card.Title><h2>Login</h2></Card.Title>
 
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              onChange={handleChangePassword}
-              value={password}
-              type="password"
-              placeholder="Enter password"
-            />
-          </Form.Group>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                onChange={handleChangeEmail}
+                value={email}
+                type="email"
+                placeholder="Enter email"
+              />
+            </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                onChange={handleChangePassword}
+                value={password}
+                type="password"
+                placeholder="Enter password"
+              />
+            </Form.Group>
+
+            <Button
+              style={{ marginTop: '10px' }}
+              variant="primary"
+              type="submit"
+            >
+              Submit
+            </Button>
+          </Form>
+
+          {
+            error.length > 0 ?
+            <Card style={{ marginTop: '20px', background: '#FFCCCB' }}>
+              <Card.Body>
+                <Card.Text>{ error }</Card.Text>
+              </Card.Body>    
+            </Card> : <div />
+          }
+
+        </Card.Body>
       </Card>
     </div>
   );
