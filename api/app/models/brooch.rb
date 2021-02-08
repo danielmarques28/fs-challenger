02798cf6aa3d4ca9e67c3e2c59cfabe162e8cb89
danielmarques28 <danielmarques7@hotmail.com>
2021-02-08 13:42:09 -0300

    feat: Add validations in Brooch model

--- api/app/models/brooch.rb
+++ api/app/models/brooch.rb
@@ -1,4 +1,8 @@
 class Brooch < ApplicationRecord
+  validates :name, :max_amount_per_user, presence: true
+  validates :name, length: { in: 5..20 }
+  validates :max_amount_per_user, numericality: { only_integer: true, greater_than: 0 }
+
   has_many :user_brooches
   has_many :users, through: :user_brooches, source: 'self_user'
 end
